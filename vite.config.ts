084d6eaaa412a740d4b16974e45554ec95b3f6f3
paulo-fs <jr.paulo87@gmail.com>
2024-04-30 08:21:18 -0300

    chore: setup import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      src: '/src',
+      app: '/src/app',
+      router: '/src/Router',
+      pages: '/src/view/pages',
+      layouts: '/src/view/layouts',
+      components: '/src/view/components',
+      utils: '/src/app/utils',
+    },
+  },
+});
